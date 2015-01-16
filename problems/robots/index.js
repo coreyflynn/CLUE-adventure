a3009327180eda6fe9b2a71f182c1c4b4a570941
Corey Flynn <flynn.corey@gmail.com>
2015-01-16 01:37:30 -0500

    added bare bones tweet module

--- problems/robots/index.js
+++ problems/robots/index.js
@@ -1,21 +1,40 @@
 var fs = require('fs');
 var path = require('path');
+var http = require('http');
 
 exports.problem = fs.createReadStream(__dirname + '/problem.txt');
 exports.solution = fs.createReadStream(__dirname + '/solution.txt');
 
 exports.verify = function (args, cb) {
-    var res = require(path.resolve(args[0]));
-    if (/beep/.test(res) && /boop/.test(res)) {
+  var apiOptions,
+      apiCallback;
+      // res = require(path.resolve(args[0]));
+
+  // set up a call to the api
+  apiOptions = {
+    host: 'api.clue.io',
+    path: 'a2/siginfo?q={"pert_desc":"sirolimus","cell_id":"MCF7"}&user_key=lincsdemo'
+  }
+  apiCallback = function(res) {
+    var resjson,
+        apiResponse = '';
+
+    res.on('data',function(chunk){
+      apiResponse += chunk;
+    });
+
+    res.on('end',function(){
+      resjson = json.parse(apiResponse);
+      if (resjson.pert_iname === 'sirolimus') {
         console.log('That sounds about right!\n');
         cb(true);
-    }
-    else if (/beep/.test(res) || /boop/.test(res)) {
-        console.log('Hmm that sounds partly convincing but try harder.\n');
+      } else {
+        console.log('That looks a little different than what we would expect!\n');
         cb(false);
-    }
-    else {
-        console.log("That doesn't sound like a robot at all.\n");
-        cb(false);
-    }
+      }
+    })
+  }
+
+  http.get(apiOptions,apiCallback);
+
 };
--- runner.js
+++ runner.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 var adventure = require('adventure');
-var shop = adventure('example-adventure');
+var shop = adventure('CLUE-adventure');
 
-var problems = [ 'dinosaurs', 'robots', 'wowsers' ];
+var problems = [ 'dinosaurs', 'robots', 'wowsers', 'tweet' ];
 problems.forEach(function (prob) {
     shop.add(prob, function () { return require('./problems/' + prob) });
 });
